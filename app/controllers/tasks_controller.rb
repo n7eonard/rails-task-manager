9c53931f49d8ea158fd2f6bfd4ac2adf11e284ba
Nicolas LEONARD <n7eonard@gmail.com>
2017-11-21 17:42:55 +0100

    As a user, I can remove a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -17,12 +19,30 @@ class TasksController < ApplicationController
     # retour à l'index
   end
 
+  def edit
+
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to task_path
+  end
+
+
 
 private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
 
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
