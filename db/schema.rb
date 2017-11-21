f833ab935b4a3b483f93bfb6d9ce7b2a276d1aa0
Nicolas LEONARD <n7eonard@gmail.com>
2017-11-21 12:43:15 +0100

    As a user, I can list tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121102819) do
+ActiveRecord::Schema.define(version: 20171121111024) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "details"
-    t.string "text"
-    t.string "completed"
-    t.string "false"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
